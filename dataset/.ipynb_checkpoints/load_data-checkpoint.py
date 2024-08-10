0e47407b58d04f2b84bae2b242a7cf1ce5c6ec44
XU ZIHANG <81083353+Zihang-Xu-2002@users.noreply.github.com>
2024-08-10 00:42:32 -0500

    Delete dataset/.ipynb_checkpoints directory

--- dataset/.ipynb_checkpoints/load_data-checkpoint.py
+++
@@ -1,9 +0,0 @@
-from datasets import load_dataset
-
-# Load all sections
-dataset = load_dataset("TrustLLM/TrustLLM-dataset")
-
-# Load one of the sections
-dataset = load_dataset("TrustLLM/TrustLLM-dataset", data_dir="robustness/ood_generalization.json")
-
-print(dataset)
