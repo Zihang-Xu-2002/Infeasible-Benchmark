8bd698965211201b96128624170dee2d37208cbf
XU ZIHANG <81083353+Zihang-Xu-2002@users.noreply.github.com>
2024-08-13 13:29:29 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,22 +1,5 @@
-# Run instructions in a foor loop :
-
-# declare -a prompt_method=("pre"  "mid_no_step" "post")
-# declare -a model=("gpt-3.5-turbo" )
-# declare -a data=("doable_task" "undoable_task")
-
-# for m in "${model[@]}"; do
-#     for p in "${prompt_method[@]}"; do
-#         for d in "${data[@]}"; do
-#             echo "Running: python -m src.generate --prompt $p --model $m --data $d --fraction 1"
-#             python -m src.generate --prompt $p --model $m --data $d --fraction 1
-#             sleep 30
-#         done
-#     done
-# done
-
 # Run instructions for a single case:
-
-# python -m src.generate --prompt pre --model gpt-3.5-turbo --data undoable_task_2k --fraction=1
+python -m src.generate --prompt pre --model gpt-3.5-turbo --data undoable_task_2k --fraction=1
 
 
 
